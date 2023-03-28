99e3657daeecca4f6e5e3420e480a50c1a98a504
Saurabh <saurabkumar679@gmail..com>
2023-03-28 18:38:53 +0530

    mocking a function learnt

--- 04-mocks/mocks.test.ts
+++ 04-mocks/mocks.test.ts
@@ -53,4 +53,26 @@ test('mock promise resolution',()=>{
 
     expect(mock('foo')).resolves.toBe('bar');
     expect(mock).toHaveBeenCalledWith('foo')
+});
+
+const forEach =(items :number[],callback:(x:number)=>number)=>{
+    for(let i=0; i<items.length; i++){
+        callback(items[i])
+    }
+}
+
+
+test('mock callbacks',()=>{
+    const mock = jest.fn(x=>x+10);
+
+    forEach([0,1,3],mock)
+
+    expect(mock.mock.calls.length).toBe(3);
+    expect(mock).toHaveBeenCalled();
+    expect(mock.mock.calls[0][0]).toBe(0);
+    expect(mock.mock.calls[1][0]).toBe(1);
+    expect(mock.mock.calls[2][0]).toBe(3);
+    expect(mock.mock.results[0].value).toBe(10);
+    expect(mock.mock.results[1].value).toBe(11);
+    expect(mock.mock.results[2].value).toBe(13);
 })
